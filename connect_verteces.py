566a7e2ab241960784eea347789bf4cf636cef07
ArthurEmig <arthur.erich.emig@gmail.com>
2022-10-23 15:45:16 +0200

    xlsx with distances added, look up this file with specified height of a room

--- connect_verteces.py
+++ connect_verteces.py
@@ -21,7 +21,26 @@ x_y_pairs_polygon = [(x, y) for (x, y) in zip(norm_list_polygon_x_coords, norm_l
 print(x_y_pairs_polygon)
 
 
+def round_to_nearest_half_int(num):
+    return round(num * 2) / 2
 
+height = 7.2
+
+height_rounded = round_to_nearest_half_int(height)
+
+df_distances = pd.read_excel("luminaires_grid_distances.xlsx")
+
+flag_high_room = height_rounded >= 6
+
+if flag_high_room:
+    wall_distance = df_distances["dist_w_l"].loc[df_distances["height_l"] == height_rounded].to_list()[0]
+    grid_distance = df_distances["dist_g_l"].loc[df_distances["height_l"] == height_rounded].to_list()[0]
+else:
+    wall_distance = df_distances["dist_wall"].loc[df_distances["height"] == height_rounded].to_list()[0]
+    grid_distance = df_distances["dist_grid"].loc[df_distances["height"] == height_rounded].to_list()[0]
+
+print(wall_distance)
+print(grid_distance)
 
 
 
